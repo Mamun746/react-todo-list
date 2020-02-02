22f607a087e6152a31e738e6c43f7f65593dc810
Mamun746 <abdullahalmamun914@gmail.com>
2020-02-02 20:03:11 +0600

    first-commit

---
+++ src/component/TodoList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import TodoItem from './TodoItem'
+
+
+
+ class TodoList extends Component {
+  state={
+      inputTodo:'',
+      todos:[]
+  }
+
+    render() {
+        return (
+            <form>
+                
+            </form>
+        )
+    }
+}
+
+export default TodoList
