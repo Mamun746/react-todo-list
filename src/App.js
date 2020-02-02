07f60caf3759d55b117000779151da036b44681a
Mamun746 <abdullahalmamun914@gmail.com>
2020-02-02 20:47:03 +0600

    design app component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import React from 'react';
-import uuid from 'uuid'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import TodoList from './component/TodoList'
-import TodoInput from './component/TodoInput'
+import React, { Component } from "react";
+import uuid from "uuid";
+import "bootstrap/dist/css/bootstrap.min.css";
+import TodoList from "./component/TodoList";
+import TodoInput from "./component/TodoInput";
 
-function App() {
-  return (
-    <div className="App">
-     <div className="container">
-     <div className="row">
-     <TodoInput/>
-     <TodoList/>
-     
-     
-     </div>
-     
-     </div>
-    </div>
-  );
+class App extends Component {
+  state = {
+    inputTodo: "",
+    todos: [
+      { id: 1, title: "wake up in the morning" },
+      { id: 2, title: "prayer" }
+    ],
+    id: uuid,
+    editTodo: false
+  };
+  handleChange = (e) => {
+    console.log("handle Change");
+  };
+  handleSubmit = (e) => {
+    console.log("handle Submit");
+  };
+  clearList = (e) => {
+    console.log("clear list");
+  };
+  handleRemove = (e) => {
+    console.log("handle remove");
+  };
+  handleEdit = (e) => {
+    console.log("handle edit");
+  };
+  re;
+  render() {
+    return (
+      <div className='container'>
+        <div className='row'>
+          <div className='col-10 mx-auto col-md-8 mt-5'>
+            <h3 className='text-capitalize text-center'>Todo List</h3>
+            <TodoInput
+              inputTodo={this.state.inputTodo}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+              editTodo={this.state.editTodo}
+            />
+            <TodoList
+              todos={this.state.todos}
+              handleRemove={this.handleRemove}
+              handleEdit={this.handleEdit}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
